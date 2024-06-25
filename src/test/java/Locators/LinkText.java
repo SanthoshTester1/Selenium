2ec415cc403d88e641a751002468ff64f5160a9d
SanthoshTester1 <gorantalasanthosh@gmail.com>
2024-06-25 17:44:45 +0530

    Locator Using LinkText

---
+++ src/test/java/Locators/LinkText.java
@@ -0,0 +1,29 @@
+package Locators;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+
+public class LinkText {
+	public static void main(String[] args) throws InterruptedException {
+		ChromeOptions chrometesting = new ChromeOptions();
+		chrometesting.setBinary("C:\\DriverPath\\ChromeTesting\\chrome-win64\\chrome.exe");
+		
+		WebDriver driver = new ChromeDriver(chrometesting);
+		driver.get("https://omayo.blogspot.com");
+		driver.manage().window().maximize();
+		
+		String linktextName = driver.findElement(By.linkText("Page One")).getText();
+		System.out.println("Link Name:- "+ linktextName);
+		
+		/*
+		WebElement Clicklink = driver.findElement(By.linkText("Page One"));
+		Clicklink.click();
+		Thread.sleep(4000);
+		*/
+		driver.close();
+		
+	}
+}
